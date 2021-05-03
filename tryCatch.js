a7a1bf7e3b8dba793701175ec8f9a5607da83905
Vinicius Estevam <vinicius.estevam99@gmail.com>
2021-05-03 15:24:11 -0300

    Try catch

---
+++ tryCatch.js
@@ -0,0 +1,13 @@
+
+let nome = "Vinicius"
+
+try {
+    if(nome == ""){
+        throw "O nome não pode ser vazio"
+    }
+    console.log(nome);
+} catch(error){
+    console.log("Houve um erro", error)
+} finally{
+    console.log("Boa noite");
+}
